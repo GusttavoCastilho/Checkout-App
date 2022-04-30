cdfc41a05899aa33031d8c9a7a9931ecdd04280d
Gustavo Castilho <gustavo.castilho@superlogica.com>
2022-04-30 10:07:33 -0400

    feat: configure mockIcons and Components to jest

--- jest/setup.ts
+++ jest/setup.ts
@@ -1,2 +1,12 @@
+import mockIcon from './mockIcon';
+
 // eslint-disable-next-line prettier/prettier
 export { };
+
+jest.mock('react-native-vector-icons/Entypo', () =>
+  mockIcon('RNVectorIcons.Entypo'),
+);
+
+jest.mock('react-native-vector-icons/AntDesign', () =>
+  mockIcon('RNVectorIcons.AntDesign'),
+);
