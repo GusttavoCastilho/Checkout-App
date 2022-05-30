fdd95ef1a9b57814b1cc9b86d27c49d48e5f0275
Gustavo Castilho <gustavo.castilho@superlogica.com>
2022-05-30 16:10:22 -0400

    feat: configure colors andmock server

--- src/global/lightTheme.ts
+++ src/global/lightTheme.ts
@@ -1,3 +1,18 @@
 export default {
-    colors: {},
+  colors: {
+    dark: '#000000',
+    dark100: '#3A3A3A',
+    dark200: '#494949',
+    dark300: '#797979',
+    primary: '#00B761',
+    gray: '#747474',
+    gray100: '#C9C9C9',
+    gray200: '#DBDBDB',
+    gray300: '#F1F1F1',
+    gray400: '#B3B3B3',
+    background: '#F3F3F3',
+    red: '#F52A2A',
+    red100: '#F3DFDF',
+    white: '#FFFFFF',
+  },
 };
